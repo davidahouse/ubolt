1663bfb617e29e536e8cdcfc9c648d0a5551f782
David House <davidahouse@gmail.com>
2019-10-27 15:23:15 -0400

    :sparkles: Multiple commands

--- bin/ubolt.js
+++ bin/ubolt.js
@@ -5,7 +5,7 @@ const pkginfo = require('pkginfo')(module);
 const yaml = require('js-yaml');
 const homedir = require('os').homedir();
 
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 
 const local = localCommands();
 const user = userCommands();
@@ -13,16 +13,21 @@ const commands = { ...user, ...local };
 const command = process.argv.length > 2 ? commands[process.argv[2]] : null;
 
 if (command != null) {
-  try {
-    var commandProcess = exec(command.command);
-    commandProcess.stdout.on('data', function(data) {
-      console.log(data);
-    });
-    commandProcess.stderr.on('data', function(data) {
-      console.log(data);
-    });
-  } catch (e) {
-    console.log('Error: ' + e);
+  if (command.command != null) {
+    try {
+      execSync(command.command, { stdio: 'inherit' });
+    } catch (e) {
+      console.log('Error: ' + e);
+    }
+  } else if (command.commands != null) {
+    for (let index = 0; index < command.commands.length; index++) {
+      const singleCommand = command.commands[index];
+      try {
+        execSync(singleCommand, { stdio: 'inherit' });
+      } catch (e) {
+        console.log('Error: ' + e);
+      }
+    }
   }
 } else {
   console.log(chalk.yellow('ubolt: ' + module.exports.version));
